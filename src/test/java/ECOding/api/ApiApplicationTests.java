e920a117466a6b54f8ae72a3f16330efba22f95e
MingyeomKim <12201863@office.inha.ac.kr>
2022-01-11 21:58:13 +0900

    Initial Commit

---
+++ src/test/java/ECOding/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ECOding.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
