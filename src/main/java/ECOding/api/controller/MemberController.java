ef1dab29b584cf34b7b4a1a4cfd91b1d0cb795fc
MingyeomKim <12201863@office.inha.ac.kr>
2022-01-15 00:16:30 +0900

    Domain 개발, Member Repository, Service, Controller 개발

---
+++ src/main/java/ECOding/api/controller/MemberController.java
@@ -0,0 +1,45 @@
+package ECOding.api.controller;
+
+import ECOding.api.domain.Member;
+import ECOding.api.repository.MemberRepository;
+import ECOding.api.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원 가입
+    @ResponseBody
+    @PostMapping("/join")
+    private Member join(@RequestBody Member member){
+        return memberService.join(member).get();
+    }
+
+    // 회원 수정
+    @ResponseBody
+    @PutMapping()
+    public Member update(@RequestBody Member member){
+        return memberService.update(member).get();
+    }
+
+    // 회원 전체 조회
+    @GetMapping
+    public List<Member> findMembers(){
+        return memberService.findMembers();
+    }
+
+    // 회원 단일 조회
+    @GetMapping
+    public Member findMember(@RequestParam Long id){
+        return memberService.findMember(id).get();
+    }
+}
+
---
+++ src/main/java/ECOding/api/domain/Member.java
@@ -0,0 +1,21 @@
+package ECOding.api.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String loginId;
+
+    private String name;
+
+    private String nickname;
+
+}
